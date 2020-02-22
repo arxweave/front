3e93092c90adcb0b262828c1dae281ffab60d415
Emmanuel Vilsbol <emmanuel@vilsbol.com>
2020-02-22 14:23:44 -0300

    Update comment

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
 export const ARXIV_BASE_URL = "https://export.arxiv.org/api"
-// add /api when using webpack 'proxy' in package.json
+// get api path from .env
 export const SW4RTZIT_API = process.env.REACT_APP_SW4RTZIT_API
